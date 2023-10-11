07d13907e8cc4847ae62a836e22cac080f81b497
ruserxd <zxcmomo0219@gmail.com>
2023-10-11 11:21:25 +0800

    coinII

--- Coin_Combinations_II.cpp
+++ Coin_Combinations_II.cpp
@@ -1,54 +1,27 @@
 #include <iostream>
-#include <string>
-#include <limits.h>
-#include <vector>
-#include <cstdlib>
 using namespace std;
-
-typedef long long int ll;
-vector<ll> dp;
-vector<ll> coins;
-const ll ma = 1e9+7;
-ll loc = 0;
-
-ll sea(ll sum) {
-    ll ans = 0;
-    if (sum == 0 ) {
-        return 1;
-    }
-    if (dp[sum] != -1) {
-        return 0;
-    }
-    for (int i = 0 ; i < coins.size(); i++) {
-        if((sum - coins[i]) >= 0) {
-            ll result = sea(sum - coins[i])%ma;
-            ans += (result %ma);
-        }
-    }
-    ans = ans % ma;
-
-    if(sum >= 0 && sum <= loc){
-        dp[sum] = ans ;
-    }
-    return ans ;
+  
+int n, x, c[105], dp[1000005], mod = 1e9+7;
+  
+void prin (int sum) {
+    for (int i = 0 ; i <= sum; i++){
+        if(dp[i]) 
+            cout << i <<"="<< dp[i] << endl;
+    } 
+    cout << endl;
 }
 int main() {
-    cin.tie(0);
-    cout.tie(0);
-    ios_base::sync_with_stdio(0);
-    ll n ,sum;
-    cin >> n >> sum;
-    coins.resize(n);
-    loc = sum;
-
-    for (int i = 0 ; i < n; i++) {
-        cin >> coins[i];
-    }   
-    dp.resize(sum+1,-1);
-    sea(sum);
-    if(dp[sum] != -1)
-        cout << dp[sum];
-    else 
-        cout << "0";
-    return 0;
+    cin >> n >> x;
+    dp[0] = 1;
+    for (int i = 0 ; i < n ;i++) cin >> c[i];
+    for (int i = 0 ; i < n ;i++) {
+        for (int k = 1; k <= x ;k++) {
+            if (k - c[i] >= 0) {
+                dp[k] += dp[k - c[i]];
+                dp[k] %= mod;
+            }
+            //prin(x);
+        }
+    }
+    cout << dp[x];
 }
